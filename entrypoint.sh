9765c7ae7924457ba02c177a7f39088f4e27b14c
virajds <viraj.ds@gmail.com>
2021-09-08 07:06:03 +0100

    APM with ddtrace-run confg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flask run --host=0.0.0.0 --port=6101
+FLASK_APP="app/app.py" D_SERVICE="recharge-input" DD_AGENT_HOST=DDTRACE_HOST DD_TRACE_AGENT_PORT="8126" DD_LOGS_INJECTION=true ddtrace-run  flask run --host=0.0.0.0 --port=6101
